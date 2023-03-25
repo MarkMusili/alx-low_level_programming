7176cceb30823502d6fa620ad76d4f14a1fbd0c5
MarkMusili <musiliyrn@gmail.com>
2023-03-25 02:32:20 -0700

    Task 3 update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 	char letters;
-	for (letters='a';letters<='z';letters++)
-		putchar(letters); 
-	for (letters='A';letters<='Z';letters++)
+
+	for (letters = 'a' ; letters <= 'z' ; letters++)
+		putchar(letters);
+	for (letters = 'A' ; letters <= 'Z' ; letters++)
 		putchar(letters);
 	putchar('\n');
-	return(0);
+	return (0);
 }
