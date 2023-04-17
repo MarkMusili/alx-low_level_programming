ddcae27fe16fb06d78d9eb13fad28698dee7d55e
MarkMusili <musiliyrn@gmail.com>
2023-04-17 19:24:08 +0000

    Define a structure called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - structure
+ *@name: member1 - name of the dog
+ *@age: member2 - age of the dog
+ *@owner: member3 - owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
