17a3ba6542b3d9720172b216bfb9636970236b99
MarkMusili <musiliyrn@gmail.com>
2023-04-21 15:13:07 +0000

    Task 3 update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,26 +1,42 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * op_add - add two integers
- * op_sub - subtract two integers
- * op_mul - multiply two integers
- * op_div - divide two integers
- * op_mod - get the modulus of two numbers
  * @a: integer 1
  * @b: integer 2
- * Return: result of the function
+ * Return: sum of two numbers
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - subtract two numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result of subtraction
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - multiply two numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Product
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - Divide two numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: exponent
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -30,9 +46,15 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+/**
+ * op_mod - Get the modulus of the two numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Modulus of the two numbers
+ */
 int op_mod(int a, int b)
 {
-	if (b = 0)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
