3c5bc993251ca0488576fddc5104b68ace15b83f
MarkMusili <musiliyrn@gmail.com>
2023-06-07 18:34:16 +0000

    Power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion- calculate the power of x raised to power y
+ * @x: Base
+ * @y: Power
+ * Return: answer or -1 if y is lower than 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
