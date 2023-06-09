274394ee6d8ce9c4bc63fa2d3acbfa0753add4fe
MarkMusili <musiliyrn@gmail.com>
2023-06-09 20:46:14 +0000

    Number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argv: array containing the command line arguments
+ * @argc: number of command line arguments passed
+ * Return: 0 for code success.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d \n", argc - 1);
+
+	return (0);
+}
