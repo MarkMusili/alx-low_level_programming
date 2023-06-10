ea60188056a3e12424ea95b0253f343ee8b184c8
MarkMusili <musiliyrn@gmail.com>
2023-06-10 12:34:02 +0000

    Program name update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,12 +8,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
 	(void)argc;
 
-	for (i = 0 ; argv[0][i] != '\0' ; i++)
-		_putchar(argv[0][i]);
-	_putchar('\n');
+	printf("%s \n", argv[0]);
 
 	return (0);
 }
