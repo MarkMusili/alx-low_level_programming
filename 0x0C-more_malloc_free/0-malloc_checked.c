c05fe537340f72d4c0a4562f6a7101c941fcf788
MarkMusili <musiliyrn@gmail.com>
2023-06-15 06:54:29 +0000

    Allocate memmory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memmory using malloc
+ * @b: size of memmory
+ * exit: 98 for normal process termination if the it fails
+ * Return: a pointer to the allocated memmory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *myptr;
+
+	myptr = malloc(b);
+	if (myptr == NULL)
+		exit(98);
+	else
+		return(myptr);
+}
