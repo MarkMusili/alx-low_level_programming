1085c8f66a2b6f847efc8efb5a2e01e2aacd9a85
MarkMusili <musiliyrn@gmail.com>
2023-06-19 20:03:17 +0000

    Sructure dog update.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - structure tag
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
 {
 	char *name;
 	float age;
