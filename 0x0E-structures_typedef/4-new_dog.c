734b55d05ebbfb119bcc13213f8f4ae2abaab380
MarkMusili <musiliyrn@gmail.com>
2023-06-19 21:37:22 +0000

    New dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * dog_t *new_dog - new structure to struct dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Return: pointer to new pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, k;
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+
+	new_dog->name = malloc(sizeof(char) * (i + 1));
+	new_dog->owner = malloc(sizeof(char) * (j + 1));
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		return (NULL);
+	}
+
+	for (k = 0; k <= i; k++)
+		new_dog->name[k] = name[k];
+	for (k = 0; k <= j; k++)
+		new_dog->owner[k] = owner[k];
+
+	new_dog->age = age;
+
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -33,4 +33,14 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
  */
 void print_dog(struct dog *d);
 
+/**
+ * new_dog - Creates a new dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog structure
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
