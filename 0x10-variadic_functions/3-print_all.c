dc56050e08b96945ea6e0bdb9e3fa47f5b8f2174
MarkMusili <musiliyrn@gmail.com>
2023-06-23 13:43:33 +0000

    Print all types

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,16 +2,65 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
+ * print_char - prints character
+ * @args: argument given
+ * Return: nothing
+ */
+void print_char(va_list args)
+{
+	char c;
+
+	c = (char)va_arg(args, int);
+	printf("%c", c);
+
+}
+/**
+ * print_int - prints an integer
+ * @args: argument given
+ * Return: nothing
+ */
+void print_int(va_list args)
+{
+	int i;
+
+	i = va_arg(args, int);
+	printf("%d", i);
+}
+
+/**
+ * print_float - prints floating point number
+ * @args: argument given
+ * Return: nothing
+ */
+void print_float(va_list args)
+{
+	double f;
+
+	f = va_arg(args, double);
+	printf("%f", f);
+}
+
+/**
+ * print_string - prints a string
+ * @args: argument given
+ * Return: nothing
+ */
+void print_string(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+/**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
  * Return: either integers, strings or characters
  */
 void print_all(const char * const format, ...)
 {
-	int i;
-	double f;
-	char *s;
-	char c;
 	va_list args;
 	const char *ptr = format;
 
@@ -21,29 +70,24 @@ void print_all(const char * const format, ...)
 		switch (*ptr)
 		{
 			case 'c':
-				c = (char) va_arg(args, int);
-				printf("%c", c);
+				print_char(args);
 				break;
 			case 'i':
-				i = va_arg(args, int);
-				printf("%d", i);
+				print_int(args);
 				break;
 			case 'f':
-				f = va_arg(args, double);
-				printf("%f", f);
+				print_float(args);
 				break;
 			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s", s);
+				print_string(args);
+				break;
+			default:
+				ptr++;
+				continue;
 		}
-		ptr++;
-		while (*ptr == 'c' || *ptr == 'i' || *ptr == 'f' || *ptr == 's')
-		{
+		if (*(ptr + 1) != '\0')
 			printf(", ");
-			break;
-		}
+		ptr++;
 	}
 	printf("\n");
 	va_end(args);
