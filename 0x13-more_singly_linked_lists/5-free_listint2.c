62283449ddd25f06b0a8540d1e193b05d923b65d
MarkMusili <musiliyrn@gmail.com>
2023-07-03 16:25:47 +0000

    Free a list and set header to

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_listint - Frees a lsist from its memmory
+ * free_listint2 - Frees a lsist from its memmory
  * @head: pointer to the list
  * Return: nothing
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *current;
 	listint_t *next;
 
-	current = head;
+	current = *head;
 	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
+	*head = NULL;
 }
