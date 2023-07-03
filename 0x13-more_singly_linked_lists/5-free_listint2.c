e5bb2ce517f390208ad45e3d1f3eb5a5ee54faf8
MarkMusili <musiliyrn@gmail.com>
2023-07-03 16:31:35 +0000

    Free a list and set header to  update.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,13 @@ void free_listint2(listint_t **head)
 	listint_t *next;
 
 	current = *head;
+
 	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
+
 	*head = NULL;
 }
