556be5e4d3ac6941db233e5b23c5a4c93c48dd3b
MarkMusili <musiliyrn@gmail.com>
2023-07-05 08:37:49 +0000

    Insert node at desired index.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	current = *head;
 	while (current != NULL && index < idx - 1)
 	{
