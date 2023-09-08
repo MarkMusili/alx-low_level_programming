a008f26465f8794ca08e89cede9e86936edcaeee
MarkMusili <musiliyrn@gmail.com>
2023-09-08 18:10:49 +0000

    Look up function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_get - retrives the value of associated key
+ * @ht: hash table to be searched
+ * @key: the key we are looking for
+ * Return: associated value with the element
+ * otherwise NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	/*Initialize the variables*/
+	unsigned long int index;
+	hash_node_t *current;
+
+	/*Get the key index*/
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	/*Loop over the nodes in the index*/
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			/*Return value*/
+			return (current->value);
+	}
+
+	/*Return NULL*/
+	return (NULL);
+
+}
