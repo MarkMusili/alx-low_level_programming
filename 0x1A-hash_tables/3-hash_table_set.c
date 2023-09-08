bef42d399abae5f4370b2a2ac2df543c0ad47b2f
MarkMusili <musiliyrn@gmail.com>
2023-09-08 19:09:36 +0000

    Insert operation update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	/*Check for invalid value*/
-	if (ht == NULL || key == NULL ||*key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	/*Generate the index*/
